2eee3abb91666e0ed588edad866db6e0b7ebaafd
marvim1990 <marcosvinim.cruz@gmail.com>
2021-02-08 15:27:41 -0300

    pag propostas

---
+++ routes/propostas.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// carregamento dos pacotes
+const express = require('express');
+const router = express.Router();
+
+const route = router.get('/', (req, res, next) => {
+    
+});
+
+module.exports = route;
