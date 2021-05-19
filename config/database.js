f229ea3244340070bc8e0c2f69ec23dfdb11f191
Cauesilvaa <43147371+Cauesilvaa@users.noreply.github.com>
2021-05-19 15:55:35 -0300

    producao analista 3 rotas

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 const config = {
-    host: 'database.grupogmvb.com',
-    username: 'dev',
-    password: 'Dev@2021',
-    database: 'base_dev',
+    host: 'database.grupogmvb.com', //18.299.217.146
+    username: 'dev', //grupogmvb
+    password: 'Dev@2021', //bruno0422LA
+    database: 'grupogmvb', //grupogmvb
     dialect: 'mysql'
   }
   
--- controllers/TblUsuarioController.js
+++ controllers/TblUsuarioController.js
@@ -9,7 +9,7 @@ const Op = Sequelize.Op;
 
 const TblUsuarioController = {
 
-    Filtro: async (req, res) => {
+    /*Filtro: async (req, res) => {
         const {
             supervisor,
             nivel
@@ -47,7 +47,92 @@ const TblUsuarioController = {
         } catch (error) {
             console.log(error)
         }
+    },*/
+
+    FiltroSetor: async (req, res) => {
+        
+        const {
+            supervisor,
+            nivel
+        } = req.body;
+
+        let where = {};
+        if (supervisor) where.supervisor = supervisor;
+        if (nivel) where.nivel = nivel;
+
+        try {
+            
+            const pesquisaSetor = await setor_usuario2.findAll({
+                where
+            });
+
+            (pesquisaSetor.length > 0 ) ? res.json({
+                pesquisaSetor,
+            }): res.json({
+                message: "Não há registros com esse filtro"
+            })
+
+        }catch (error) {
+            console.log(error)
+        }
     },
+
+    FiltroMensal: async (req, res) => {
+
+        const {
+            supervisor,
+            nivel
+        } = req.body;
+
+        let where = {};
+        if (supervisor) where.supervisor = supervisor;
+        if (nivel) where.nivel = nivel;
+
+        try {
+
+            const pesquisaMes = await usuario_mes2.findAll({
+                where
+            });
+
+            (pesquisaMes.length > 0 ) ? res.json({
+                pesquisaMes,
+            }): res.json({
+                message: "Não há registros com esse filtro"
+            })
+
+        }catch (error) {
+            console.log(error)
+        }
+
+    },
+
+    Dia: async (req, res) => {
+        const {
+            supervisor,
+            nivel
+        } = req.body;
+
+        let where = {};
+        if (supervisor) where.supervisor = supervisor;
+        if (nivel) where.nivel = nivel;
+
+        try {
+
+            console.time("Tempo de resposta da 3 consultas");
+
+            //Setor
+            const pesquisaDia = await usuario_dia2.findAll({
+                where
+            });
+
+            console.timeEnd("Tempo de resposta da 3 consultas");
+
+            return res.json({pesquisaDia: pesquisaDia});
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
 }
 module.exports = TblUsuarioController;
